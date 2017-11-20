65cca91fd13134779253cc500ab2c8a2ddcdb1b4
Pavel Rudnev <RudnevPR@it.mos.ru>
2017-11-20 15:26:07 +0300

    end project

--- bitbot.js
+++ bitbot.js
@@ -1,13 +1,25 @@
-var TelegramBot = require('node-telegram-bot-api');
-var token = '494529036:AAFsmmpqUq9kyYTPG5Crl03XcnyLSA-Z4SA';
-var bot = new TelegramBot(token, {polling: true});
-var todaydate = new Date();
+var parser = require('./parser.js');
+const TelegramBot = require('node-telegram-bot-api');
+const token = '494529036:AAFsmmpqUq9kyYTPG5Crl03XcnyLSA-Z4SA';
+const bot = new TelegramBot(token, {polling: true});
+//var todaydate = new Date();
 //var correctdate = todaydate.substring(0,9);
-console.log(todaydate);
+//console.log(todaydate);
 //0-9
 
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
-    console.log(msg);
-    bot.sendMessage(chatId, "Hello!", {caption: "I'm a bot!"});
+// Matches "/echo [whatever]"
+bot.onText(/\/royalty/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const tomorrow = 'Доходность завтра: '+parser.massiv[0];
+  const today ='Доходность сегодня: '+ parser.massiv[1] ;
+  const yesterday ='Доходность вчера: '+parser.massiv[2] ;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, tomorrow);
+  bot.sendMessage(chatId, today);
+  bot.sendMessage(chatId, yesterday);
 });
--- parser.js
+++ parser.js
@@ -1,11 +1,46 @@
 var request = require('request'), cheerio = require('cheerio');
 
 //Загружаем страницу
-request({uri:'https://bitconnect.co/learning-center/bitconnect-bitcoin-price-volatility-software', method:'GET', encoding:'binary'},
+request({uri:'https://bitconnect.co/learning-center/bitconnect-bitcoin-price-volatility-software', method:'GET', encoding:'UTF-8'},
     function (err, res, page) {
         //Передаём страницу в cheerio
         var $=cheerio.load(page);
         //Идём по DOM-дереву обычными CSS-селекторами
-        img_src=$('div.s9a3 > div > div > a > div > div > img').attr("src");
-        console.log(img_src);
+        //img_src=$('#col-xs-4 col-sm-2 text-center p10').text();
+        var rendering = $.html('.col-xs-4.col-sm-2.text-center.p10 strong');
+        //var redertext = cheerio.text($('<div class="м" style="border:1px dashed #484848"></div>'))
+        //console.log(typeof(img_src));
+        //console.log(img_src+' ');
+        //console.log(rendering);
+        var arrofroyal = rendering.split('g>').map(function(item, i, arr) {
+        	item = arr[i].match("\.\.\.\.%");
+        	if(item) {
+        		return item.toString();
+        	}
+
+        	return null;
+	    });
+
+        //console.log(arrofroyal);
+        module.exports.massiv = arrofroyal;
     });
+ 
+
+
+/*<div class="col-xs-4 col-sm-2 text-center p10" style="border:1px dashed #484848">
+  <div>
+    <strong style="font-size: 18px">1.06%
+    </strong>
+  </div>
+  <div>
+   <i class="fa fa-calendar"></i> 2017-11-21</div>
+  <div><i class="fa fa-clock-o text-warning fa-lg"></i> Pending </div>
+</div>
+*/
+
+ /*<strong style="font-size: 18px">1.06%</strong>
+ <strong style="font-size: 18px">0.55%</strong>
+ <strong style="font-size: 18px">0.77%</strong>
+ <strong style="font-size: 18px">1.50%</strong>
+ <strong style="font-size: 18px">1.95%</strong>
+ <strong style="font-size: 18px">0.48%</strong>*/
