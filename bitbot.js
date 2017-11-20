ef09cc588786b675bb1afeb12ab698b2155d6da2
Pavel Rudnev <RudnevPR@it.mos.ru>
2017-11-20 15:28:50 +0300

    too many comments

--- bitbot.js
+++ bitbot.js
@@ -2,23 +2,18 @@ var parser = require('./parser.js');
 const TelegramBot = require('node-telegram-bot-api');
 const token = '494529036:AAFsmmpqUq9kyYTPG5Crl03XcnyLSA-Z4SA';
 const bot = new TelegramBot(token, {polling: true});
-//var todaydate = new Date();
-//var correctdate = todaydate.substring(0,9);
-//console.log(todaydate);
-//0-9
 
-// Matches "/echo [whatever]"
+
+
 bot.onText(/\/royalty/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
+
 
   const chatId = msg.chat.id;
   const tomorrow = 'Доходность завтра: '+parser.massiv[0];
   const today ='Доходность сегодня: '+ parser.massiv[1] ;
   const yesterday ='Доходность вчера: '+parser.massiv[2] ;
 
-  // send back the matched "whatever" to the chat
+  
   bot.sendMessage(chatId, tomorrow);
   bot.sendMessage(chatId, today);
   bot.sendMessage(chatId, yesterday);
--- parser.js
+++ parser.js
@@ -6,12 +6,8 @@ request({uri:'https://bitconnect.co/learning-center/bitconnect-bitcoin-price-vol
         //Передаём страницу в cheerio
         var $=cheerio.load(page);
         //Идём по DOM-дереву обычными CSS-селекторами
-        //img_src=$('#col-xs-4 col-sm-2 text-center p10').text();
+        //$('#col-xs-4 col-sm-2 text-center p10').text();
         var rendering = $.html('.col-xs-4.col-sm-2.text-center.p10 strong');
-        //var redertext = cheerio.text($('<div class="м" style="border:1px dashed #484848"></div>'))
-        //console.log(typeof(img_src));
-        //console.log(img_src+' ');
-        //console.log(rendering);
         var arrofroyal = rendering.split('g>').map(function(item, i, arr) {
         	item = arr[i].match("\.\.\.\.%");
         	if(item) {
@@ -27,20 +23,3 @@ request({uri:'https://bitconnect.co/learning-center/bitconnect-bitcoin-price-vol
  
 
 
-/*<div class="col-xs-4 col-sm-2 text-center p10" style="border:1px dashed #484848">
-  <div>
-    <strong style="font-size: 18px">1.06%
-    </strong>
-  </div>
-  <div>
-   <i class="fa fa-calendar"></i> 2017-11-21</div>
-  <div><i class="fa fa-clock-o text-warning fa-lg"></i> Pending </div>
-</div>
-*/
-
- /*<strong style="font-size: 18px">1.06%</strong>
- <strong style="font-size: 18px">0.55%</strong>
- <strong style="font-size: 18px">0.77%</strong>
- <strong style="font-size: 18px">1.50%</strong>
- <strong style="font-size: 18px">1.95%</strong>
- <strong style="font-size: 18px">0.48%</strong>*/
